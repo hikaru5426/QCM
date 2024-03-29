6334e4b1f77ca06bdbbaef2374a98af4dc30c996
Tony <moliniertony1@gmail.com>
2024-03-29 15:42:20 +0100

    Debuguer should work LOL

--- Main.php
+++ Main.php
@@ -1,40 +1,44 @@
 <?php
 
+//session_id
 session_start();
 $servername = 'localhost';
-$username = 'root';
-$password = '';
+$usernamePHPMyadmin = 'root';
+$passwordPHPMyadmin = '';
+$pSaisieLoginMDP = "Vous devez obligatoirement saisir un login et mot de passe pour entrer sur l'application QCM";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['login'])){
+        try{
+            $connLogin = new PDO("mysql:host=$servername;dbname=qcm", $usernamePHPMyadmin, $passwordPHPMyadmin);
+            $checkUser = $connLogin->prepare("SELECT * FROM utilisateur WHERE Login = :login AND MotDePasse = :mdp");
+            $checkUser->bindParam(':login', $_POST['login']);
+            $checkUser->bindParam(':mdp', $_POST['mdp']);
+            $checkUser->execute();
+            $user = $checkUser->fetchAll(PDO::FETCH_ASSOC);
 
-if (isset($_POST['login'])){
-    try{
-        $connLogin = new PDO("mysql:host=$servername;dbname=qcm", $username, $password);
-        $checkUser = $connLogin->prepare("SELECT * FROM utilisateur WHERE Login = :login AND MotDePasse = :mdp");
-        $checkUser->bindParam(':login', $_POST['login']);
-        $checkUser->bindParam(':mdp', $_POST['mdp']);
-        $checkUser->execute();
-        $user = fetchAll(PDO::FETCH_ASSOC);
+            if ($checkUser->fetchColumn() == 1){
+                
+                $_SESSION['idUtilisateur'] = $user[0]['idUtilisateur'] ;
+                $_SESSION['login'] = $user[0]['login'] ;
+                $_SESSION['motDePasse'] = $user[0]['motDePasse'] ;
+                $_SESSION['nom'] = $user[0]['nom'] ;
+                $_SESSION['prénom'] = $user[0]['prénom'] ;
+                var_dump($_SESSION['idUtilisateur']);
+                var_dump($_SESSION['login']);
+                var_dump($_SESSION['motDePasse']);
+                var_dump($_SESSION['nom']);
+                var_dump($_SESSION['prénom']);
 
-        if ($checkUser->fetchColumn() == 1){
+                header("Login.php");
+            } else{
+                $pSaisieLoginMDP = "Login ou mot de passe incorrecte";
+                header("Location: ". $_SERVER['PHP_SELF']);
+            }
             
-            $_SESSION['idUtilisateur'] = $user[0]['idUtilisateur'] ;
-            $_SESSION['login'] = $user[0]['login'] ;
-            $_SESSION['motDePasse'] = $user[0]['motDePasse'] ;
-            $_SESSION['nom'] = $user[0]['nom'] ;
-            $_SESSION['prénom'] = $user[0]['prénom'] ;
-            var_dump($_SESSION['idUtilisateur']);
-            var_dump($_SESSION['login']);
-            var_dump($_SESSION['motDePasse']);
-            var_dump($_SESSION['nom']);
-            var_dump($_SESSION['prénom']);
-
-            header("Login.php");
-        } else{
-
         }
-        
-    }
-    catch(PDOException $e){
-        echo "e.getMessage() = " . $e->getMessage();
+        catch(PDOException $e){
+            echo "e.getMessage() = " . $e->getMessage();
+        }
     }
 }
 
@@ -49,7 +53,7 @@ if (isset($_POST['login'])){
     <body>
         <h1 class="centered blue">Accueil du site QCM :</h1>
 
-        <form action="login.php" method="POST">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
             <div class="formAccueil">
                 <span class="bold">Login :</span><input type="text" name="login"><br><br>
                 <span class="bold">Mot de passe :</span><input type="text" name="mdp"><br><br>
@@ -58,6 +62,6 @@ if (isset($_POST['login'])){
             <div class="centered"><input type="submit" value="OK"></div>
         </form>
 
-        <p class="centered red">Vous devez obligatoirement saisir un login et mot de passe pour entrer sur l'application QCM</p>
+        <p class="centered red"><?php echo $pSaisieLoginMDP ?></p>
     </body>
 </html>
